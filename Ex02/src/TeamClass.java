08f01446365ad906012ec60f69ee689b582ce7c1
KHJ <clcjs987@gmail.com>
2025-04-28 14:25:45 +0900

    팀원3 김형진 커밋

--- Ex02/src/TeamClass.java
+++ Ex02/src/TeamClass.java
@@ -6,7 +6,7 @@ public class TeamClass {
 		System.out.println("1번 팀장_김희광 커밋");
 		System.out.println("2번 팀원 김다원 커밋");
 		System.out.println("3번 팀원 김수연 커밋");
-		
+		System.out.println("4번 팀원 김형진 커밋");
 		
 		
 	}
