bc1be0798430aa04fbb965bfbd46f4067cccd851
김희광 <gmlrhkd432@gmail.com>
2025-04-30 15:34:46 +0900

    ASCII 아트(몬스터) 완료_김희광

---
+++ Epic Tower Quest/src/ascill/Shadow.java
@@ -0,0 +1,58 @@
+package ascill;
+
+public class Shadow {
+
+	public static void main(String[] args) {
+		  System.out.println("                                               ");
+	        System.out.println("                                               ");
+	        System.out.println("                   ░░░░                       ");
+	        System.out.println("                 ░▒▒▒▒▒▒░                     ");
+	        System.out.println("                 ▒▓▓▓▓▓▓▒                     ");
+	        System.out.println("                ▒▓▓▓▒▒▓▓▓▒                    ");
+	        System.out.println("                ▓▓▓▒▒▒▒▓▓▓                    ");
+	        System.out.println("               ░▓▓▒▒▒▒▒▒▓▓░                   ");
+	        System.out.println("               ░▓▓▒▒▒▒▒▒▓▓░                   ");
+	        System.out.println("               ░▓▓▒▒▒▒▒▒▓▓░                   ");
+	        System.out.println("                ▓▓▓▓▒▒▓▓▓▓                    ");
+	        System.out.println("                ░▓▓▓▒▒▓▓▓░                    ");
+	        System.out.println("                ▒▓▓▓▒▒▓▓▓▒                    ");
+	        System.out.println("               ▒▓▓▓▒▒▒▒▓▓▓▒                   ");
+	        System.out.println("             ░▒▓▓▓▒▒▒▒▒▒▓▓▓▒░                 ");
+	        System.out.println("            ░▒▓▓▓▒▒▒▒▒▒▒▒▓▓▓▒░                ");
+	        System.out.println("           ░▒▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▒░               ");
+	        System.out.println("          ░▒▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▒░              ");
+	        System.out.println("          ▒▓▓▓▒▒▓▒▒▒▒▒▒▒▓▓▓▓▓▓▓▒              ");
+	        System.out.println("         ▒▓▓▓▒▒▓▓▓▒▒▒▒▒▓▓▓▓▓▓▓▓▓▒             ");
+	        System.out.println("        ▒▓▓▓▒▒▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▒            ");
+	        System.out.println("      ░▒▓▓▓▒▒▓▓▓▓▓▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒░          ");
+	        System.out.println("     ░▒▓▓▓▒▒▓▓▓▓▓▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░         ");
+	        System.out.println("     ░▓▓▓▒▒▓▓▓▒░▓▓▒▓▓▓▓▓▓▓░▒▓▓▓▓▓▓▓▓░         ");
+	        System.out.println("    ░▒▓▓▒▒▓▓▓▒░░▓▓▓▓▓▓▓▓▓▓░░▒▓▓▓▓▓▓▓▒░        ");
+	        System.out.println("    ░▒▓▓▒▓▓▓▒░─░▓▓▓▓▓▓▓▓▓▓░─░▒▓▓▓▓▓▓▒░        ");
+	        System.out.println("    ░▒▓▓▓▓▓▒░──░▓▓▓▓▓▓▓▓▓▓░──░▒▓▓▓▓▓▒░        ");
+	        System.out.println("     ░▒▓▓▓▒░───▒▓▓▓▓▓▓▓▓▓▓▒───░▒▓▓▓▒░         ");
+	        System.out.println("      ░░░░░───░▓▓▓▓▓▓▓▓▓▓▓▓░───░░░░░          ");
+	        System.out.println("                ▒▓▓▓▓▓▓▓▓▓▓▓▒                 ");
+	        System.out.println("               ░▒▓▓▓▓▓▓▓▓▓▓▓▓▒░               ");
+	        System.out.println("               ▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒               ");
+	        System.out.println("              ░▒▓▓▓▓▓▓░░▓▓▓▓▓▓▒░              ");
+	        System.out.println("              ░▓▓▓▓▓▓▓──▓▓▓▓▓▓▓░              ");
+	        System.out.println("             ░░▓▓▓▓▓▓░──░▓▓▓▓▓▓░░             ");
+	        System.out.println("             ░▓▓▓▓▓▓▓░──░▓▓▓▓▓▓▓░             ");
+	        System.out.println("             ░▓▓▓▓▓▓▒────▒▓▓▓▓▓▓░             ");
+	        System.out.println("            ░▓▓▓▓▓▓▓──────▓▓▓▓▓▓▓░            ");
+	        System.out.println("            ░▓▓▓▓▓▓▒──────▒▓▓▓▓▓▓░            ");
+	        System.out.println("            ▒▓▓▓▓▓▓░──────░▓▓▓▓▓▓▒            ");
+	        System.out.println("            ▓▓▓▓▓▓▓────────▓▓▓▓▓▓▓            ");
+	        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+	        System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+	        System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+	        System.out.println("          ░▓▓▓▓▓▓▓▒░       ░▓▓▓▓▓▓▓▒            ");
+	        System.out.println("         ░▒▓▓▓▓▓▓▒░       ░▓▓▓▓▓▓▓▒░            ");
+	        System.out.println("        ░▒▓▓▓▓▓▓▒░       ░▓▓▓▓▓▓▓▒░             ");
+	        System.out.println("       ░▓▓▓▓▓▓▓▒░       ░▓▓▓▓▓▓▓▒░              ");
+	        System.out.println("      ░▓▓▓▓▓▓▓░░       ░▓▓▓▓▓▓▓▒░               ");
+	        System.out.println("     ░▓▓▓▓▓▓▓░░       ░▓▓▓▓▓▓▓▒░                ");
+	}
+
+}
---
+++ Epic Tower Quest/src/ascill/wizard.java
@@ -0,0 +1,39 @@
+package ascill;
+
+public class wizard {
+
+	public static void main(String[] args) {
+		 System.out.println("    ░░        ░    ░░    ░  ░░");
+	        System.out.println("     ░░░  ░         ░░ ░  ");
+	        System.out.println("       ░      ░░   ░ ░   ░  ");
+	        System.out.println("  ░░ ░░░░░  ░░  ░░░░░░ ░ ░   ░");
+	        System.out.println("  ░░░░▓▓▒░░░   ░ ░░     ░░░ ░ ");
+	        System.out.println("  ░░░▒███░░░░░░ ░     ░ ░░ ░░");
+	        System.out.println("   ░░▒███▓░░▒▒▓░ ░   ░   ░  ░░");
+	        System.out.println("░░ ░░ ████▒▓███░░░   ░  ░░ ░░ ");
+	        System.out.println(" ░  ░░████████▓░░░░░░   ░░░  ░");
+	        System.out.println("   ░░▓████████░░  ░░    ░░   ");
+	        System.out.println("   ▒███████████░░░░░░   ░    ");
+	        System.out.println(" ░ ░████████████▓░░░▒░░     ░░");
+	        System.out.println("░░░▒▒████████▓████▒▓▒▓░░░░   ░");
+	        System.out.println("   ░░████████▒▒█████░█▓░░  ░  ");
+	        System.out.println("  ░ ░█████████░▓██████▓▒░░░   ");
+	        System.out.println("  ░ ░▓████████▒▒██████░▒░░░   ");
+	        System.out.println("░░░ ▓██████████▓████████▓░    ");
+	        System.out.println(" ▒░░░███████████▓███████▓░░   ");
+	        System.out.println(" ▒░░█████████████████████▒▒░░░");
+	        System.out.println("░░█░████████▓▓████████████▓▓░░");
+	        System.out.println("░▓▓▓████████▓▓▓█████████████▒░");
+	        System.out.println("░▒██████████▓▓▓▓███████████▓▒░");
+	        System.out.println("░▒░▒████████▓▓▓▓▓████████▓▓▒░░");
+	        System.out.println("░░░█████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░");
+	        System.out.println("░▒░█████████▓▓▒▒▓▓▓▓▓▓▓▒▒▒▒░░░");
+	        System.out.println("░▒▒████████▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒░░░░");
+	        System.out.println("░░▓███████▓▓▓▒▒▒▒▒░░░░░░░░░░░ ");
+	        System.out.println(" ░▒▓▓▓████▓▓▒▒░░░░░░░░░░░░░░░ ");
+	        System.out.println(" ░░▒▒▒▒▓▓▓▓▒░░░░░░░░░░░░░░░░  ");
+	        System.out.println(" ░░░░░░░▒░░░░░░░░░░░░░░░░░    ");
+	        System.out.println("  ░░░░░░░░░░░░░░              ");
+	}
+
+}
